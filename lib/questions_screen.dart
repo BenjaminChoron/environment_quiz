973ac20cbc79dcb9db6cbcc85d721dbce61823f4
BenjCH <benjamin.choron@outlook.com>
2024-06-28 15:10:25 +0200

    🚧 Display question and options

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
+import 'package:environment_quiz/questions.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,6 +12,25 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Questions');
+    final currentQuestion = questions[0];
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          currentQuestion.question,
+          style: GoogleFonts.montserrat(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 30),
+        ...currentQuestion.options.map((option) {
+          return Text(option);
+        })
+      ],
+    );
   }
 }
